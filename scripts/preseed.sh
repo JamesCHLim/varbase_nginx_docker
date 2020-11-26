271db3c7a85c904f56ea675169e00cd375abcac7
James C. H. Lim <james.lim.ori@gmail.com>
2020-11-26 11:49:17 +1100

    removed obsolete compose files, changing project to work with an imported site instead of a new one.

--- scripts/preseed.sh
+++ scripts/preseed.sh
@@ -1,1 +1,11 @@
-docker-compose run --rm --no-deps -- drupal tar -cC /var/www/html/sites . | tar -xC ./varbase/sites
+#!/bin/bash
+set -eux
+
+CONTAINER_NAME="${1:-james/varbase-base:0.1}"
+#CONTAINER_NAME="james/varbase-base:0.1"
+
+for folder in "modules" "profiles" "sites" "themes"
+do
+    mkdir -p "./drupal/${folder}"
+    docker run --rm "$CONTAINER_NAME" tar -cC "/var/www/html/${folder}" . | tar -xC "./drupal/${folder}"
+done
